ba994e0dd6ccf2976b2ea3146f9683d16f3275d5
James Cooke <jamescooke@users.noreply.github.com>
2019-10-06 12:14:51 +0100

    Add a simple unique boolean func

--- src/ufaker/ufaker.py
+++ src/ufaker/ufaker.py
@@ -1,5 +1,8 @@
 from typing import Dict
+
 from faker import Faker
+
+from .exceptions import GeneratorExhausted
 
 
 class uFaker:
@@ -8,4 +11,20 @@ class uFaker:
         self.faker = Faker()
 
     def boolean(self):
-        return self.faker.boolean()
+        attempts = 0
+        while True:
+            value = self.faker.boolean()
+
+            try:
+                used = value in self._caches["boolean"]
+            except KeyError:
+                self._caches["boolean"] = {value}
+                return value
+
+            if not used:
+                self._caches["boolean"].add(value)
+                return value
+
+            attempts += 1
+            if attempts >= 200:
+                raise GeneratorExhausted()
